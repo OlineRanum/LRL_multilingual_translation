820f0bbda57b0ed4c62e55f7a6ba7e8c0c1faec2
OlineRanum <oline.a.ranum@gmail.com>
2023-09-22 13:56:01 +0200

    Added environment + installation of xnmt and langrank

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # DLNLP_Project
 Project on multilingual translation for DLNLP
+
+## Installation
+
+``` Installing and configuring repo
+git clone https://github.com/OlineRanum/DLNLP_Project.git
+cd DLNLP_Project
+
+conda env create -f env.yml
+conda activate dlnlp
+
+bash setup.sh
+```
---
+++ setup_ubuntu_gpu.sh
@@ -0,0 +1,38 @@
+
+# Ensure cmake is available for compiling dynet
+pip install cmake
+## Install Lightgmb
+conda install -c conda-forge lightgbm
+
+# Lang2Vec installation
+git clone https://github.com/antonisa/lang2vec.git
+cd lang2vec
+wget http://www.cs.cmu.edu/~aanastas/files/distances.zip .
+mv distances.zip lang2vec/data/
+python3 setup.py install
+cd ../
+
+# Langrank installation
+git clone https://github.com/neulab/langrank.git
+cd langrank 
+pip install -r requirements.txt
+wget http://phontron.com/data/langrank/indexed.tar.gz  .
+tar -xzvf indexed.tar.gz
+rm indexed.tar.gz
+cd ../
+
+# XNMT
+git clone https://github.com/neulab/xnmt.git
+cd xnmt
+pip install -r requirements.txt
+python setup.py install
+
+# Install correct version of protobuf
+pip uninstall protobuf
+pip install protobuf==3.20.0
+
+
+# END
+python << END
+print('------- Set-up for DLNLP-project complete -------\n')
+END

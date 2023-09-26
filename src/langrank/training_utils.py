5a4687d650e54d40607e533b575e2fde0572f857
jjhl <jjhlgraber@gmail.com>
2023-09-26 17:23:09 +0200

    Start of implementing training ranker

---
+++ src/langrank/training_utils.py
@@ -0,0 +1,84 @@
+from scipy.stats import rankdata
+import numpy as np
+import os
+
+# globals, hardcoded for the single language joint training from the paper
+I2L = [
+    "ara",
+    "aze",
+    "bel",
+    "ben",
+    "bos",
+    "bul",
+    "ces",
+    "cmn",
+    "dan",
+    "deu",
+    "ell",
+    "epo",
+    "est",
+    "eus",
+    "fas",
+    "fin",
+    "fra",
+    "glg",
+    "heb",
+    "hin",
+    "hrv",
+    "hun",
+    "hye",
+    "ind",
+    "ita",
+    "jpn",
+    "kat",
+    "kaz",
+    "kor",
+    "kur",
+    "lit",
+    "mar",
+    "mkd",
+    "mon",
+    "msa",
+    "mya",
+    "nld",
+    "nob",
+    "pol",
+    "por",
+    "ron",
+    "rus",
+    "slk",
+    "slv",
+    "spa",
+    "sqi",
+    "srp",
+    "swe",
+    "tam",
+    "tha",
+    "tur",
+    "ukr",
+    "urd",
+    "vie",
+]
+l2I = {l: i for i, l in enumerate(I2L)}
+RAW_DIR = os.path.join("ground_truth_rankings", "raw")
+ORIGINAL_PAPER_GROUND_TRUTH_FILENAME = (
+    "LangRank Transfer Language Raw Data - MT Results.csv"
+)
+
+
+def get_ranking_from_raw_score(
+    dir=RAW_DIR,
+    raw_scores_file_name=ORIGINAL_PAPER_GROUND_TRUTH_FILENAME,
+):
+    file_path = os.path.join(dir, raw_scores_file_name)
+    ground_truth = np.genfromtxt(
+        file_path,
+        delimiter=",",
+        skip_header=1,
+        usecols=range(1, len(I2L) + 1),
+        dtype=float,
+    )[:-2]
+    ordinal_ranking = ground_truth.shape[0] - rankdata(
+        ground_truth, method="ordinal", axis=0
+    )
+    return ordinal_ranking

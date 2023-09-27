c536f6b3e95a869c6097f5766d7be75bd0d6889a
OlineRanum <oline.a.ranum@gmail.com>
2023-09-27 10:42:09 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,5 +1,2 @@
-import xnmt
-from xnmt import xnmt_run_experiment
-
-experiment = xnmt_run_experiment.RunExp(config_file="train_preproc.yaml")
-experiment.run()
+import os
+os.system("xnmt train_preproc.yaml")

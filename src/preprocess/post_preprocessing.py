9897b1b91c5784b903d4231675c21daafd3b625d
OlineRanum <oline.a.ranum@gmail.com>
2023-09-29 15:45:12 +0200

    edit post processing tools

--- src/preprocess/post_preprocessing.py
+++ src/preprocess/post_preprocessing.py
@@ -3,7 +3,7 @@ def replace_spaces_with_underscore(input_file, output_file):
         with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
             for line in infile:
                 # Replace spaces with "__" and write to the output file
-                modified_line = line.replace(' ', '__')
+                modified_line = line.replace(' ', ' __ ')
                 outfile.write(modified_line)
     except FileNotFoundError:
         print("Input file not found.")

45be664afaf134586c1e7a03f595801c21e04242
SW0BBR <alex.labro@student.uva.nl>
2023-10-05 15:33:24 +0200

    GPU implementation

--- train.py
+++ train.py
@@ -1,2 +1,5 @@
 import os
-os.system("xnmt train_preproc.yaml")
+import torch
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("This file will be running on", device)
+os.system("xnmt --backend torch --gpu train_preproc.yaml")

8f126d760b0c4ee0f2ae8b1c60734ba0efd2cb57
SW0BBR <alex.labro@student.uva.nl>
2023-10-12 16:36:19 +0200

    Cleaning up

--- train.py
+++
@@ -1,5 +0,0 @@
-import os
-import torch
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print("This file will be running on", device)
-os.system("xnmt --backend torch --gpu train_preproc.yaml")
